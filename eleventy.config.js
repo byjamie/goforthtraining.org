e498b2516c9b37a3484c264d078d20e0f47c24bc
Jamie Tan <jamie@gravoso.com>
2025-07-16 18:18:56 +0800

    Add Markdown plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import markdownIt from "markdown-it";
 
 export default function (eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
@@ -6,6 +7,14 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/fonts");
     eleventyConfig.addWatchTarget("src/css");
+
+    /* Markdown config */
+    let options = {
+        html: true, // Enable HTML tags in source
+        linkify: true, // Auto-convert URL-like text to links
+        typographer: true, // Enable language-neutral replacement + quotes beautification. Full list of replacements: https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.mjs
+    };
+    eleventyConfig.setLibrary("md", markdownIt(options));
 };
 
 export const config = {
